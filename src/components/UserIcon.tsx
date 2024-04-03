8556256d47f560124c19e3a5f547863f59dc8fbe
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-04-03 21:39:53 +0900

    add picture tag

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -6,13 +6,15 @@ type Props = {
 
 export const UserIcon = ({ id, image, name }: Props) => {
   return (
-    <img
-      src={
-        image ??
-        `https://source.boringavatars.com/beam/120/${id ?? ""}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51`
-      }
-      alt={name ?? ""}
-      className="rounded-full border bg-cover"
-    />
+    <picture>
+      <img
+        src={
+          image ??
+          `https://source.boringavatars.com/beam/120/${id ?? ""}?colors=264653,2a9d8f,e9c46a,f4a261,e76f51`
+        }
+        alt={name ?? ""}
+        className="rounded-full border bg-cover"
+      />
+    </picture>
   );
 };
